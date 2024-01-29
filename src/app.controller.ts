7ded408f834c175bca455bf2a9a62c66119ccd61
Mohammad Goudarzi <mohammad.goudarzi@glue-labs.com>
2024-01-29 12:45:06 +0100

    create dedicated cat resource

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,40 +1,12 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-} from '@nestjs/common';
-import { AppService, Cat } from './app.service';
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
-@Controller('cats')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  findAll(): Cat[] {
-    return this.appService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string): Cat {
-    return this.appService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() cat: Cat) {
-    return this.appService.update(+id, cat);
-  }
-
-  @Delete(':id')
-  delete(@Param('id') id: string): void {
-    return this.appService.delete(+id);
-  }
-
-  @Post()
-  create(@Body() cat: Cat) {
-    return this.appService.create(cat);
+  @Get('ping')
+  ping() {
+    return 'pong';
   }
 }
---
+++ src/cats/cats.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
+import { Cat } from './entities/cat.entity';
+
+@Injectable()
+export class CatsService {
+  constructor() {
+    this.cats.push({ id: 1, name: 'Cat 1', age: 1 });
+    this.cats.push({ id: 2, name: 'Cat 2', age: 2 });
+    this.cats.push({ id: 3, name: 'Cat 3', age: 3 });
+  }
+
+  private readonly cats: Cat[] = [];
+
+  create(createCatDto: CreateCatDto) {
+    this.cats.push(createCatDto);
+  }
+
+  findAll() {
+    return this.cats;
+  }
+
+  findOne(id: number) {
+    const cat = this.cats.find((cat) => cat.id === id);
+    return cat;
+  }
+
+  update(id: number, updateCatDto: UpdateCatDto) {
+    this.cats.map((cat) => {
+      if (cat.id === id) {
+        cat.age = updateCatDto.age;
+        cat.name = updateCatDto.name;
+        return cat;
+      } else {
+        return cat;
+      }
+    });
+    return this.cats;
+  }
+
+  remove(id: number) {
+    const catIndex = this.cats.findIndex((cat) => cat.id === id);
+    const cat = this.cats[catIndex];
+    this.cats.splice(catIndex, 1);
+    return;
+  }
+}
